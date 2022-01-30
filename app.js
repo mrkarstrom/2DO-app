e9c54abcc3e353a956836c77b48e7ae4a0930118
Magnus Karström <magnus@yesweb.se>
2022-01-30 18:10:03 +0100

    Set up HTML, working on app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-app.js
+let data = (localStorage.getItem('todoList')) ? 
+JSON.parse(localStorage.getItem('todoList')) 
+: {
+    todo: [],
+    completed: [],
+};
+
+renderTodoList();
+document.getElementById('add').addEventListener('click', function() {
+    let value = document.getElementById('item').value;
+    if(value) {
+        addItem(value);
+    }
+});
+
+document.getElementById('item').addEventListener('keydown', function (e) {
+    let value = this.value;
+    if ((e.code === 'Enter' || e.code === 'NumpadEnter') && value) {
+        addItem(value);
+    }
+});
+
